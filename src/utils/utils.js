6c377da00990bd866099d649b94573cfa1bc3c5f
escorpivo <vamigg00@estudiantes.unileon.es>
2024-05-10 03:50:37 +0200

    Update: few changes

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -39,7 +39,7 @@ export const subscribeToNewsletter = async (email) => {
   });
 
   const requestOptions = {
-    method: "POST",
+    method: "GET",
     headers,
     body,
     redirect: "follow",
